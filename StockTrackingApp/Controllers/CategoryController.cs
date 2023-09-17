1519e77e9a2f81da02adfc7152e1f0c986305d29
gamzeguzleobuz <gamzeguzle@gmail.com>
2023-09-17 18:57:03 +0300

    listing categories

--- StockTrackingApp/Controllers/CategoryController.cs
+++ StockTrackingApp/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StockTrackingApp.Context;
+using StockTrackingApp.Data;
 
 namespace StockTrackingApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly StockDbContext _db;
+
+        public CategoryController(StockDbContext db) 
+        {
+            _db = db;
+        }    
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Categories.ToList());
         }
     }
 }
